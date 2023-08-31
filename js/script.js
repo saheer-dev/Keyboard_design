0b47913b65ce78c77c45f2a7cf317f0f5f39ba7c
Saheer k <143588271+saheer-dev@users.noreply.github.com>
2023-08-31 20:24:42 +0530

    fifth commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let space = document.querySelector('.keyboard')
+function show(n){
+    space.innerText+=n
+}
+function dlt(){
+    space.innerText=""
+}
+function spc(a){
+    space.innerText+=a
+}
